d8d965be17ba357b45ef268bf3052beccbb81db5
Maureen-Njeri <maureenwangarinjeri@gmail.com>
2024-09-20 19:35:53 +0300

    made changes to navbar linking

--- landvista-informationalwebsite/src/app/components/Landing/index.tsx
+++ landvista-informationalwebsite/src/app/components/Landing/index.tsx
@@ -40,7 +40,7 @@ const LandingPage: React.FC = () => {
   }, [slides.length]);
 
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className={`relative w-full h-screen overflow-hidden ${poppins.className}`}>
       {slides.map((slide, index) => (
         <div
           key={index}
@@ -57,16 +57,16 @@ const LandingPage: React.FC = () => {
           />
           <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center">
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-70 p-8 md:p-10 lg:p-12 rounded-lg w-[85%] max-w-[900px] z-20">
-              <h2 className="text-[#CA7558] text-2xl md:text-3xl lg:text-4xl font-bold mb-5">
+              <h2 className="text-[#CA7558] text-3xl md:text-4xl lg:text-5xl font-bold mb-6">
                 {slide.title}
               </h2>
-              <div className="flex mb-5">
+              <div className="flex mb-6">
                 <div className="w-1 bg-red-500 mr-4"></div>
-                <p className="text-gray-700 text-base md:text-lg lg:text-xl">
+                <p className="text-gray-700 text-lg md:text-xl lg:text-2xl">
                   {slide.subText}
                 </p>
               </div>
-              <button className="bg-teal-600 hover:bg-teal-700 text-white py-2 px-4 rounded-full transition duration-300 text-lg">
+              <button className="bg-teal-600 hover:bg-teal-700 text-white py-3 px-5 rounded-full transition duration-300 text-xl">
                 {slide.buttonText}
               </button>
             </div>
--- landvista-informationalwebsite/src/app/components/Navbar/index.tsx
+++ landvista-informationalwebsite/src/app/components/Navbar/index.tsx
@@ -16,18 +16,18 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="relative bg-white shadow-md hover:bg-gray-100 transition duration-300 px-16 h-16">
+    <nav className={`relative bg-white shadow-md hover:bg-gray-100 transition duration-300 px-16 h-16 ${poppins.className}`}>
       <div className="relative">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
             <Image 
-    src="/images/logo.png" 
-    alt="logo" 
-    width={180} 
-    height={60} 
-    className="w-full h-auto max-w-[180px] md:max-w-[180px] md:h-auto"
-    layout="responsive"
-  />
+              src="/images/logo.png" 
+              alt="logo" 
+              width={180} 
+              height={60} 
+              className="w-full h-auto max-w-[180px] md:max-w-[180px] md:h-auto"
+              layout="responsive"
+            />
           </div>
 
           <div className="absolute top-4 right-1 md:hidden z-50">
@@ -38,7 +38,7 @@ export default function Navbar() {
             </button>
           </div>
 
-          <div className="hidden md:flex items-center space-x-8 text-lg lg:text-xl xl:text-3xl text-black" style={{ fontFamily: '"Darker Grotesque"' }}>
+          <div className="hidden md:flex items-center space-x-8 text-lg lg:text-xl xl:text-3xl text-black">
             <Link href="/" className="text-black hover:text-[#CA7558] hover:underline py-1 px-2 transition duration-300">Home</Link>
             <Link href="#about" className="text-black hover:text-[#CA7558] hover:underline py-1 px-2 transition duration-300">About Us</Link>
             <Link href="#overview" className="text-black hover:text-[#CA7558] hover:underline py-1 px-2 transition duration-300">Overview</Link>
@@ -60,8 +60,7 @@ export default function Navbar() {
               onClick={() => setIsOpen(false)}
             ></div>
             <div 
-              className="fixed inset-0 bg-white opacity-70 z-40 flex flex-col justify-center text-lg text-black px-5" 
-              style={{ fontFamily: '"Darker Grotesque"' }}
+              className={`fixed inset-0 bg-white opacity-70 z-40 flex flex-col justify-center text-lg text-black px-5 ${poppins.className}`}
             >
               <div className="space-y-5">
                 <Link href="/" className="block text-black hover:text-[#CA7558] hover:underline py-1 px-2 transition duration-300" onClick={() => setIsOpen(false)}>Home</Link>
